6278cef3a93016878da60a17baa1fd6a36a5869f
ntacey <ntacey@hotmail.com>
2016-07-26 08:00:43 -0700

    Refactored two dimensional arrays into NumHolder objects for simplicity

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /* 
+ * @Author ntacey
+ * @Date 7/6/2016
+ *
+ * A project to help learn es6 concepts. Would like to eventually
+ * convert into a node app.
+ *
  * Main function solveSudoku takes in an 81 byte number
  * and returns a 81 byte number which is the solved puzzle
  *
@@ -6,8 +12,17 @@
 
 function solveSudoku(inputNumbers){ 
     
+    // Class for creating objects that hold the current squares
+    // index value as well as its numeric value (0 - 9)
+    class NumHolder {
+        constructor(indVal, numVal) {
+            this.indVal = indVal;
+            this.numVal = numVal;
+        }
+    }
+    
     const parsedInputNumbers = []
-    var solvingNumberMap = [] // a two dimensional array ie a map that holds the position value (starting at 1 not 0) as well as the corresponding value
+    var solvingNumberIndex = [] // a two dimensional array ie a map that holds the position value (starting at 1 not 0) as well as the corresponding value
     const solvedInputNumbers = []
     var row1 = []
     var row2 = []
@@ -58,86 +73,88 @@ function solveSudoku(inputNumbers){
         let currNum = parsedInputNumbers[i];
         let divNum;
         
+        let currNumHolder = new NumHolder(i + 1, currNum);
+        
         //generate rows
         if (i < 9) {
-            row1.push([i+1], currNum);
+            row1.push(currNumHolder);
             divNum = i;
         } else if (i >= 9 & i < 18) {
-            row2.push([i+1], currNum);
+            row2.push(currNumHolder);
             divNum = (i - 9);
         } else if (i >= 18 & i < 27) {
-            row3.push([i+1], currNum);
+            row3.push(currNumHolder);
             divNum = (i - 18);
         } else if (i >= 27 & i < 36) {
-            row4.push([i+1], currNum);
+            row4.push(currNumHolder);
             divNum = (i - 27);
         } else if (i >= 36 & i < 45) {
-            row5.push([i+1], currNum);
+            row5.push(currNumHolder);
             divNum = (i - 36);
         } else if (i >= 45 & i < 54) {
-            row6.push([i+1], currNum);
+            row6.push(currNumHolder);
             divNum = (i - 45);
         } else if (i >= 54 & i < 63) {
-            row7.push([i+1], currNum);
+            row7.push(currNumHolder);
             divNum = (i - 54);
         } else if (i >= 63 & i < 72) {
-            row8.push([i+1], currNum);
+            row8.push(currNumHolder);
             divNum = (i - 63);
         } else if (i >= 72 & i < 81) {
-            row9.push([i+1], currNum);
+            row9.push(currNumHolder);
             divNum = (i - 72);
         }
         
         //generate columns
         if (divNum === 0) {
-            col1.push([i+1], currNum);
+            col1.push(currNumHolder);
         } else if (divNum === 1) {
-            col2.push([i+1], currNum);
+            col2.push(currNumHolder);
         } else if (divNum === 2) {
-            col3.push([i+1], currNum);
+            col3.push(currNumHolder);
         } else if (divNum === 3) {
-            col4.push([i+1], currNum);
+            col4.push(currNumHolder);
         } else if (divNum === 4) {
-            col5.push([i+1], currNum);
+            col5.push(currNumHolder);
         } else if (divNum === 5) {
-            col6.push([i+1], currNum);
+            col6.push(currNumHolder);
         } else if (divNum === 6) {
             col7.push([i+1]currNum);
         } else if (divNum === 7) {
             col8.push([i+1]currNum);
         } else if (divNum === 8) {
-            col9.push([i+1], currNum);
+            col9.push(currNumHolder);
         }
         
         //generate nonets
         if (i < 27) {
             if (divNum < 3) {
-                nonet1.push([i+1], currNum);
+                nonet1.push(currNumHolder);
             } else if (divNum >= 3 & divNum < 6) {
-                nonet2.push([i+1], currNum);
+                nonet2.push(currNumHolder);
             } else {
-                nonet3.push([i+1], currNum);
+                nonet3.push(currNumHolder);
             }
         } else if (i >= 27 & i < 54) {
             if (divNum < 3) {
-                nonet4.push([i+1], currNum);
+                nonet4.push(currNumHolder);
             } else if (divNum >= 3 & divNum < 6) {
-                nonet5.push([i+1], currNum);
+                nonet5.push(currNumHolder);
             } else {
-                nonet6.push([i+1], currNum);
+                nonet6.push(currNumHolder);
             }
         } else if (i >= 54 & i < 81) {
             if (divNum < 3) {
-                nonet7.push([i+1], currNum);
+                nonet7.push(currNumHolder);
             } else if (divNum >= 3 & divNum < 6) {
-                nonet8.push([i+1], currNum);
+                nonet8.push(currNumHolder);
             } else {
-                nonet9.push([i+1], currNum);
+                nonet9.push(currNumHolder);
             }
         }
         
-        //populate solvingNumbersMap
-        solvingNumberMap.push([i+1], currNum]);
+        //populate solvingNumbersIndex
+        solvingNumberIndex.push(currNumHolder);
     }
     
     /* removed until necessary. this function removes 0s in row, col, or nonet array. so [0, 1, 9, 0, 7, 0] -> [1, 9, 7]
@@ -161,13 +178,9 @@ function solveSudoku(inputNumbers){
         //loop through nonets..
         for (let nonet of nonetList) {
             //loop through values in current nonet..
-            for (var i = 0; i < (nonet.length/2); i++) {
+            for (var i = 0; i < (nonet.length); i++) {
                 //since this is a two dimensional array, skip every other value
-                if (! (i % 2) === 0) {
-                    if (nonet[i] === 0) {
-                        compareCrossRowsAndCols(nonet[i-1]);
-                    }
-                }
+                nonetList
             }
         }
     }
@@ -192,26 +205,30 @@ function solveSudoku(inputNumbers){
             solvePuzz();
         }
     }
+                
+    //note: all error handling will probably be moved to its own js file
+    function InvalidInputException(type) {
+        this.type = type;
+        this.name = "InvalidInputException";
+    
+        switch(type) {
+            case 0:
+                this.message = "Too few numbers in input";
+            case 1:
+                this.message = "Too many numbers in input";
+            case 2:
+                this.message = "Invalid character in input string";
+            default:
+                this.message = "Unknown input exception";
+        }
+    
+    }
+        
 
 }
 
 
 
-//note: all error handling will probably be moved to its own js file
-function InvalidInputException(type) {
-   this.type = type;
-   this.name = "InvalidInputException";
-    
-   switch(type) {
-       case 0:
-           this.message = "Too few numbers in input";
-       case 1:
-           this.message = "Too many numbers in input";
-       case 2:
-           this.message = "Invalid character in input string";
-       default:
-           this.message = "Unknown input exception";
-   }
-    
-}
+
+
 
